ad1cc7c5a8c45083dbdd7e3b96270a4f7b7d2186
gersonRamirez22 <ramirezriosgerson@gmail.com>
2022-01-28 15:10:12 -0500

    add logic programming summary

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//Logica de programacion
+
+//Variables
+//Tipos de datos(strings y number booleanos charecteres)
+//operadores(aritmeticos, relacionales, logicos)
+//funciones
+//ciclos(for, while, do while, for in for of)
+
+
